df7e344ab7f8a5ae9cac554353733b2ae33c35a1
nicolaparutto <nicolaparutto2003@gmail.com>
2025-01-20 17:05:20 +0100

    feat: small part of the exercise completed

--- src/components/form.jsx
+++ src/components/form.jsx
@@ -1,59 +1,83 @@
 import { useState } from 'react';
 
-const articles = [
-   {
-      id: 1,
-      text: "Oggi si è registrato un record storico di temperature in Europa, con 20 gradi sopra la media stagionale."
-   },
-   {
-      id: 2,
-      text: "Un nuovo studio scientifico rivela che l'intelligenza artificiale potrebbe rivoluzionare la diagnosi delle malattie rare."
-   },
-   {
-      id: 3,
-      text: "La NASA ha annunciato il lancio di una nuova missione verso Marte per cercare tracce di vita passata."
-   }
-];
+const defaultArticle = {
+   id: '',
+   title: '',
+   content: '',
+   image: '',
+   category: [],
+   tags: [],
+   state: ''
+};
+
+let articlesList = [];
 
 function Form() {
 
-   const [articlesList, setArticlesList] = useState(articles);
-   const [newArticle, setNewArticle] = useState({ text: '' })
-
+   const [article, setArticle] = useState({} = defaultArticle);
+   const [articles, setArticles] = useState(articlesList);
    const handlerSubmit = (e) => {
       e.preventDefault();
-      const newArticlesList = [newArticle, ...articlesList]
-      setArticlesList(newArticlesList)
+      //logica invio form
+      console.log(article)
+      setArticles(articlesList.push(article))
+      console.log(articlesList)
    }
+
+   //nuovo articolo:
    const handlerNewArticle = (e) => {
-      const newArticle = {
-         id: Date.now(),
-         text: e.target.value
-      }
-      setNewArticle(newArticle);
+      let { name, value } = e.target;
+
+      setArticle({
+         ...article,
+         [name]: value
+      })
    }
-   const handlerRemoveArticle = (id) => {
-      const updatedArticleList = articlesList.filter(article => (article.id !== id))
-      setArticlesList(updatedArticleList)
-   }
+   //rimuovi articolo:
+   // const handlerRemoveArticle = (id) => {
+   //    const updatedArticleList = articlesList.filter(article => (article.id !== id))
+   //    setArticlesList(updatedArticleList)
+   // }
 
    return (
       <div className="container form-container">
          <form action="" onSubmit={handlerSubmit}>
             <input
                type="text"
-               placeholder="Inserisci il testo dell'articolo"
-               value={newArticle.text}
+               placeholder='Titolo...'
+               name='title'
+               value={article.title}
                onChange={handlerNewArticle}
-               required />
+               required
+            />
+            <input
+               type="text"
+               placeholder="Inserisci il testo dell'articolo"
+               name='content'
+               value={article.content}
+               onChange={handlerNewArticle}
+               required
+            />
+            <select name="category" id="category">
+               <option value="1">Intelligenza Artificiale</option>
+               <option value="2">Innovazioni Tecnologiche</option>
+               <option value="3">Software e App</option>
+               <option value="4">Moda e tendenze</option>
+               <option value="5">Scuola e Istruzione</option>
+               <option value="6">Viaggi e Turismo</option>
+            </select>
+
+
+
             <button className="btn" type="submit">Genera</button>
          </form>
 
          <div className="articles-section">
-            {articlesList.map((article) => (
+            {articlesList.map(article => (
                <div key={article.id} className="article">
                   <div className="article-text">
-                     <p>{article.text}</p>
+                     <h1>{article.title}</h1>
+                     <p>{article.content}</p>
                   </div>
                   <div
                      className="article-trash"
