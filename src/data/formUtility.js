a4391ea694575d5459799748266921fb3e027357
nicolaparutto <nicolaparutto2003@gmail.com>
2025-01-21 17:13:51 +0100

    build: add data folder with form utility

---
+++ src/data/formUtility.js
@@ -0,0 +1,24 @@
+//Categories array:
+const categories = [
+   "Intelligenza Artificiale",
+   "Innovazioni Tecnologiche",
+   "Software e App",
+   "Moda e tendenze",
+   "Scuola e Istruzione",
+   "Viaggi e Turismo"
+]
+
+//Tags array:
+const tags = [
+   { id: 1, name: "cultura" },
+   { id: 2, name: "mondo" },
+   { id: 3, name: "notizie" },
+   { id: 4, name: "innovazioni" },
+   { id: 5, name: "istruzione" },
+   { id: 6, name: "viaggi" }
+]
+
+export {
+   categories,
+   tags
+}
